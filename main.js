973d45c14641ec7988c09c9feb5ccc8d3fd93fde
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-04-20 17:33:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,168 @@ function get(id) {
 }
 function getDefaultSave() {
 	return {
-		matter: 0,
-		nanites: 1,
-		naniteCost: 10,
-		int: 0,
+		ideas: new Decimal(1),
+		thinkers: [
+			"empty",
+			{
+				amount:new Decimal(0),
+				cost:new Decimal(10),
+				mult:new Decimal(1),
+				bought:new Decimal(0),
+				costMult:new Decimal(1.15)
+			},
+			{
+				amount:new Decimal(0),
+				cost:new Decimal(100),
+				mult:new Decimal(1),
+				bought:new Decimal(0),
+				costMult:new Decimal(1.2)
+			},
+			{
+				amount:new Decimal(0),
+				cost:new Decimal(10000),
+				mult:new Decimal(1),
+				bought:new Decimal(0),
+				costMult:new Decimal(1.3)
+			},
+			{
+				amount:new Decimal(0),
+				cost:new Decimal(1000000),
+				mult:new Decimal(1),
+				bought:new Decimal(0),
+				costMult:new Decimal(1.35)
+			},
+			{
+				amount:new Decimal(0),
+				cost:new Decimal(1e9),
+				mult:new Decimal(1),
+				bought:new Decimal(0),
+				costMult:new Decimal(1.375)
+			},
+			{
+				amount:new Decimal(0),
+				cost:new Decimal(1e11),
+				mult:new Decimal(1),
+				bought:new Decimal(0),
+				costMult:new Decimal(1.4)
+			},
+			{
+				amount:new Decimal(0),
+				cost:new Decimal(100),
+				mult:new Decimal(1),
+				bought:new Decimal(0),
+				costMult:new Decimal(1.41)
+			},
+			{
+				amount:new Decimal(0),
+				cost:new Decimal(1000),
+				mult:new Decimal(1),
+				bought:new Decimal(0),
+				costMult:new Decimal(1.42)
+			}
+			],
+		exist: new Decimal(0),
 		lastTick: new Date().getTime(),
-		escapes: 0,
+		creations: new Decimal(0),
 	}
+}
+
+let player = getDefaultSave();
+
+function gameLoop() {
+	let newTime = new Date().getTime()
+	let diff = (newTime - player.lastTick) / 1000;
+	player.lastTick = newTime;
+	produce(diff);
+	update();
+}
+
+function produce(time) {
+	player.ideas = player.thinkers[1].times(time).add(player.ideas);
+	for(let i = 1; i < 8; i++) {
+		player.thinkers[i] = player.thinkers[i+1].times(time).add(player.ideas);
+	}
+}
+
+function update() {
+	get("ideas").innerHTML = player.ideas.toString();
+	let unlocked7 = false;
+	for(let i = 1; i <= 8; i++) {
+		get("tier"+i+"Amount").innerHTML = player.thinkers[i].amount.floor().toString();
+		get("tier"+i+"Mult").innerHTML = player.thinkers[i].mult.toString();
+		get("tier"+i+"Cost").innerHTML = player.thinkers[i].cost.floor().toString();
+		if(i <= 6) {
+			if(player.creations.gte(i-1)) {
+				get("tier"+i).style.display = "";
+			} else {
+				get("tier"+i).style.display = "none";
+			}
+		} else {
+			if(player.exist.gte(player.thinkers[i].cost) || player.thinkers[i].amount.gt(0)) {
+				get("tier"+i).style.display = "";
+				unlocked7 = true;
+			} else {
+				get("tier"+i).style.display = "none";
+			}
+		}
+	}
+	if(unlocked7) {
+		get("weakMaxAll").style.display = "";
+	} else {
+		get("weakMaxAll").style.display = "none";
+	}
+	if(existOnCreate().gte(1)) {
+		get("creation").style.display = "";
+		get("existOnCreate").style.display = existOnCreate().floor().toString();
+	} else {
+		get("creation").style.display = "none";
+	}
+}
+
+function existOnCreate() {
+	return player.ideas.div(100).log2();
+}
+function buyTier(tier) {
+	if(player.ideas.gte(player.thinkers[tier].cost) && tier <= 6) {
+		player.thinkers[tier].amount = player.thinkers[tier].amount.add(1);
+		player.ideas = player.ideas.sub(player.thinkers[tier].cost);
+		player.thinkers[tier].cost = player.thinkers[tier].cost.times(player.thinkers[tier].costMult);
+	} else if(player.exist.gte(player.thinkers[tier].cost)) {
+		player.thinkers[tier].amount = player.thinkers[tier].amount.add(1);
+		player.exist = player.exist.sub(player.thinkers[tier].cost);
+		player.thinkers[tier].cost = player.thinkers[tier].cost.times(player.thinkers[tier].costMult);
+	}
+}
+function buyMaxTier(tier) {
+	if(tier <= 6) {
+		while(player.ideas.gte(player.thinkers[tier].cost)) {
+			buyTier(tier);
+		}
+	} else {
+		while(player.exist.gte(player.thinkers[tier].cost)) {
+			buyTier(tier);
+		}
+	}
+}
+function creation() {
+	player.ideas = new Decimal(10);
+	for(let i = 1; i <= 6; i++) {
+		player.thinkers[i].amount = new Decimal(0);
+		player.thinkers[i].bought = new Decimal(0);
+	}
+	player.thinkers[1].cost = new Decimal(10);
+	player.thinkers[2].cost = new Decimal(100);
+	player.thinkers[3].cost = new Decimal(10000);
+	player.thinkers[4].cost = new Decimal(1000000);
+	player.thinkers[5].cost = new Decimal(1e9);
+	player.thinkers[6].cost = new Decimal(1e11);
+	
+	player.thinkers[1].costMult = new Decimal(1.15);
+	player.thinkers[2].costMult = new Decimal(1.2);
+	player.thinkers[3].costMult = new Decimal(1.3);
+	player.thinkers[4].costMult = new Decimal(1.35);
+	player.thinkers[5].costMult = new Decimal(1.375);
+	player.thinkers[6].costMult = new Decimal(1.4);
+	player.creations++;
+	player.exist = player.exist.add(existOnCreate());
 }
