a26f2296cdac20c8f188d3a13c31be625df524fa
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-04-21 15:24:25 -0500

    Update saving.js

--- saving.js
+++ saving.js
@@ -1,10 +1,12 @@
-function save(player) {
+function save() {
 	localStorage.setItem(btoa('universe_creation'), JSON.stringify(player, function(k, v) { return (v === Infinity) ? "Infinity" : v; }));
+	get("saveButton").innerHTML = "Saved!";
+	setTimeout(changeSaveButton, 5000);
 }
 function load() {
 	let save = JSON.parse(localStorage.getItem(btoa('universe_creation')));
 	cleanSave(save,getDefaultSave());
-	startInterval();
+	start();
 }
 
 function cleanSave(thing,defaultThing) {
@@ -20,3 +22,36 @@ function cleanSave(thing,defaultThing) {
 		}
 	}
 }
+function exp() {
+	let output = get("exportOutput");
+	let parent = output.parentElement;
+
+	parent.style.display = "";
+	output.value = localStorage.getItem(btoa('universe_creation'));
+
+	output.onblur = function() {
+		parent.style.display = "none";
+	}
+	try {
+		if (document.execCommand('copy')) {
+			get("exportButton").innerHTML = "Copied to clipboard";
+			output.blur();
+			setTimeout(changeExport, 5000);
+		}
+	} catch(ex) {
+		// well, we tried.
+	}
+}
+function changeExport() {
+	get("exportButton").innerHTML = "Export";
+}
+function changeSaveButton() {
+	get("saveButton").innerHTML = "Save";
+}
+function imp() {
+	var saveData = prompt("Input your save. Your current save file will be overwritten!");
+	saveData = atob(saveData);
+	localStorage.setItem(btoa('universe_creation'), saveData);
+	load();
+}
+setInterval(save,5000);
